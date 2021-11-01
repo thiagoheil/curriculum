6de001c8be1e1a2ec0bccd8f1fc11cbd77dbc792
thiagoheil <thiago.heilmann@hotmail.com>
2021-11-01 11:48:09 -0300

    refactor da tela meus projetos

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Projetos from "./pages/Projetos";
 import Main from "./pages/Main";
+import HomeSgurdWebsite from "./pages/Projetos/projects/sgurd-website/Home/Home";
 
 function Routes() {
   return (
@@ -10,6 +11,7 @@ function Routes() {
       <Switch>
         <Route path="/projetos" exact render={() => <Projetos />} />
         <Route path="/" exact render={() => <Main />} />
+        <Route path="/sgurd-home" exact render={() => <HomeSgurdWebsite />} />
       </Switch>
     </Router>
   );
